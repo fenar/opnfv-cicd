425164b80221921bdf9fb3e3e82d1073e313da57
fenar@yahoo.com <fenar@yahoo.com>
2017-06-09 17:20:47 -0500

    updates

---
+++ 04-configure-jumphost-for-jenkins-user.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+set -ex
+
+
+# configure jumphost for jenkins user
+cj() {
+    set -ex
+    export DEBIAN_FRONTEND=noninteractive
+
+    while [ ! -z "$(sudo lsof /var/lib/apt/lists/lock)"  ]
+    do
+        echo "Waiting for dpkg/apt lock..."
+        sleep 3s
+    done
+    mkdir ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    ssh-keygen -t rsa
+    touch ~/.ssh/authorized_keys
+    chmod 600 ~/.ssh/authorized_keys
+    cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+    mkdir ~/slave_root
+    git config --global user.email "fenar@yahoo.com"
+    git config --global user.name "fenar"
+}
+#sshpass -p 'jenkins' ssh jenkins@localhost "$(cat);configure_jumphost"
+sshpass -p 'jenkins' ssh jenkins@localhost "$(typeset -f); cj"
